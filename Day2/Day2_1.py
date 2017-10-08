01bd46c7d2ddcc813cbde173a08b39fa11b592b5
Noyon <noyonmassive@gmail.com>
2017-10-08 21:09:00 +0600

    Create Day2_1.py
    Basic Python

---
+++ Day2/Day2_1.py
@@ -0,0 +1,10 @@
+print("hello python")
+'''
+single comment
+'''
+print('this is first line\n'
+      '          this is second line\n'
+      '          this is \n third line\n'
+      '          ')
+
+
