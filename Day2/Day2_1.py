b06ca992849faf0c91637ce6418e7ca25f9db5d6
Noyon <noyonmassive@gmail.com>
2017-10-08 21:16:01 +0600

    Update Day2_1.py
    Basic python variable, type cast, user input

--- Day2/Day2_1.py
+++ Day2/Day2_1.py
@@ -7,4 +7,89 @@ print('this is first line\n'
       '          this is \n third line\n'
       '          ')
 
+a=10
+b=20
+c=a+b
+print('a value:'+ str(a)+'+'+'b value:'+str(b)+ '=' + str(a+b))
+
+
+print("================================================")
+
+
+
+d="hello "
+e="world"
+f=d+e
+print(f)
+
+print("=================================================")
+
+
+
+k=True
+print('this is k value',id(k))
+
+
+print("================================================")
+
+'''
+
+a=int(input('a value:'))
+b=int(input('b value'))
+c=int(input('c value'))
+print(c+b+a)
+
+
+print("================================================")
+
+
+
+a=int(input('a value:'))
+b=int(input('b value'))
+c=int(input('c value'))
+print(c*b*a)
+'''
+print('================================================')
+
+#print(43/54)
+#print(34//54)
+
+help(pow)
+help('symbols ')
+
+
+
+print("===================================================")
+
+
+
+print(ord('A'))     #65
+print(chr(65))     #A
+print(int('12'))    #12
+print(float('12.23'))   # 12.23
+print(str(12))       # '12'
+print(bool('String'))  #True
+
+print(int('python'))    #Error
+
+print(bool(0))    # False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
