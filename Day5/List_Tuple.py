d4070a54f297f017206e720a3e554e609c373128
Noyon <noyonmassive@gmail.com>
2017-10-15 20:52:43 +0600

    Create List_Tuple.py
    Python List [], Tuple ()

---
+++ Day5/List_Tuple.py
@@ -0,0 +1,55 @@
+
+# a=eval(input())
+# print(a)
+
+# a=int(input('First Value'))
+# b=int(input('Second Value'))
+
+# print(eval(input('calculator:')))
+
+# l=list(2,3,4,5)
+# l=[2,3,4,5]
+# print(l)
+
+
+# list=[2,'two',3.3,True]
+# list=[2,'two',3.3,True,[2,3,4,5]]
+# print(list[4][0])
+
+# text='hello world'
+# list=[2,3,[1,2,[text,3,4,'hello']]]
+# print(list[2][2][3])
+
+# fru=['mango','mango',True,'orange','lem',[2,3,4]]
+# fru.append('dem')
+# fru[4].append(['hello',3,4])
+# fru[4].extend('hello world')
+# fru[4].extend(['hello',3,4])
+# fru.remove('mango')
+# fru.clear()
+
+# fru_copy=fru
+# fru_copy=fru.copy()
+# fru_copy.append('12')
+# print(fru)
+# fru.insert(2,'good')
+# print(fru)
+# print(fru.index(True))
+
+
+
+
+# t=tuple('smo shajib')
+# o=1,2,3
+# p=(1,2,3)
+# print(o)
+
+
+m={2,3,4}
+n=set('hello')
+print(n)
+
+# help('list')
+
+
+
