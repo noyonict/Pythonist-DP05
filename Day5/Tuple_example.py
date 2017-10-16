3a018be048a85606e35ddd0de5183fc3602c719e
Noyon <noyonmassive@gmail.com>
2017-10-16 18:49:26 +0600

    Create Tuple_example.py

---
+++ Day5/Tuple_example.py
@@ -0,0 +1,19 @@
+t1 = ()
+t2 = "Name"
+t3 = "Name",
+t4 = 3, 4, 5
+t5 = (3, 4, 5)
+t6 = tuple([2, 3, 4, 5, 'Name'])
+print(t1)
+print(t2)
+print(t3)
+print(t4)
+print(t5)
+print(t6)
+
+print(type(t1))
+print(type(t2))
+print(type(t3))
+print(type(t4))
+print(type(t5))
+print(type(t6))
