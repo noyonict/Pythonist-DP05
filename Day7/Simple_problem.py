d41147f604f34e064411a948fb1ac77570cc1cad
Noyon <noyonmassive@gmail.com>
2017-10-18 19:02:47 +0600

    Create Simple_problem.py
    Python 3. # Integer number Reversed. # Remove duplicate value from list, # convert decimal to others

---
+++ Day7/Simple_problem.py
@@ -0,0 +1,29 @@
+# Integer number Reversed
+n = int(input('Number: '))
+num1 = int(str(n)[::-1])
+print(num1)
+print(type(num1))
+
+# Remove duplicate value from list
+l = [1, 2, 3, 4, 65, 76, 89, 23, 43, 1, 2, 43, 43, 2, 1, 3, 4, 4, 4, 5, 89]
+print(list(set(l)))
+
+# convert decimal to others
+num = 13
+binary = '110101'
+print('Binary = ', bin(num))
+print('Binary = {:b}'.format(num))
+print('Octal = ', oct(num))
+print('Binary = {:o}'.format(num))
+print('Hexa = ', hex(num))
+print('Binary = {:x}'.format(num))
+
+
+
+
+
+
+
+
+
+
